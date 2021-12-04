d9114c8e9c3987cda5c2051fd9618bcc31fc564e
Adam von Arnim <vonarnim@usc.edu>
2021-12-03 21:59:25 -0800

    boron

---
+++ boron/Compost-o-Matic/target/3.0.0/boron/obj/src/newlib_stubs.o.d
@@ -0,0 +1,152 @@
+/Users/adamvonarnim/Desktop/Compost-O-Matic/Compost-o-Matic/target/3.0.0/boron//obj/./src/newlib_stubs.o: \
+ src/newlib_stubs.cpp \
+ ../../../modules/shared/nRF52840/inc/user-part/newlib_stubs.inc \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/types.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/cdefs.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_types.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_types.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/lock.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/endian.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_endian.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/select.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_sigset.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timeval.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/timespec.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timespec.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/sched.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/types.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/stdlib.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/cstdlib \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/c++config.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/os_defines.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/cpu_defines.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdlib.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/reent.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/stdlib.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/alloca.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/xlocale.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/bits/std_abs.h \
+ ../../../services/inc/debug.h ../../../services/inc/service_debug.h \
+ ../../../services/inc/logging.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/string.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/strings.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/string.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h \
+ /Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ ../../../services/inc/panic.h ../../../services/inc/panic_codes.h \
+ ../../../services/inc/config.h ../../../services/inc/preprocessor.h
+
+../../../modules/shared/nRF52840/inc/user-part/newlib_stubs.inc:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/types.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/newlib-nano/newlib.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_newlib_version.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/config.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/ieeefp.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/features.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/cdefs.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_default_types.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_types.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_types.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/lock.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_stdint.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/endian.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/_endian.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/select.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_sigset.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timeval.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/timespec.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_timespec.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/sched.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/types.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/stdlib.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/cstdlib:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/c++config.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/os_defines.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/arm-none-eabi/thumb/v7e-m+fp/hard/bits/cpu_defines.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdlib.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/_ansi.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/reent.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/machine/stdlib.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/alloca.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/xlocale.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/c++/9.2.1/bits/std_abs.h:
+
+../../../services/inc/debug.h:
+
+../../../services/inc/service_debug.h:
+
+../../../services/inc/logging.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/string.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/strings.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/string.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/stdint.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/arm-none-eabi/include/sys/_intsup.h:
+
+/Users/adamvonarnim/.particle/toolchains/gcc-arm/9.2.1/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h:
+
+../../../services/inc/panic.h:
+
+../../../services/inc/panic_codes.h:
+
+../../../services/inc/config.h:
+
+../../../services/inc/preprocessor.h:
