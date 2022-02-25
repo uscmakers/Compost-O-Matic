5e12b1aa42ce75e35c5121fb2a34d299a94a06e9
mginsy <mnginsbe@usc.edu>
2022-02-25 10:52:25 -0800

    just backend

--- boron/Compost-o-Matic/target/3.0.0/boron/obj/src/user_module.o.d
+++
@@ -1,8 +0,0 @@
-/Users/adamvonarnim/Desktop/Compost-O-Matic/Compost-o-Matic/target/3.0.0/boron//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-
-../../../dynalib/inc/dynalib.h:
