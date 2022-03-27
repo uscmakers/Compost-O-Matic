01122440129dd403dd400acb7864f5900501f1db
ayushimi <ayushimi@usc.edu>
2022-03-27 14:31:06 -0700

    Gets and displays data from Node server

--- client/src/App.js
+++ client/src/App.js
@@ -5,31 +5,57 @@ import { Line, defaults } from 'react-chartjs-2';
 import { fetchData } from './Firebase.js';
 import Dropdown from './Dropdown';
 
-function generateXLabelsAndTimestamps(interval,numPoints) {
-    var timestamps = []
-    var xLabels = []
-    var currMil = new Date(2021, 11, 5);
-    var currRounded = Math.round(currMil / 1000 / 60 / interval) * interval * 60 * 1000;
-    if (currRounded > currMil) {
-        currRounded -= interval*60000; // decrement by interval mins
+function generateXLabelsAndTimestamps(numDays, data) {
+    let timestamps = []
+    let xLabels = []
+    data = data.reverse();
+    let currDay = new Date(data[0]);
+    let dayCount = 1;
+    let skip = 1;
+    if (numDays >= 15) {
+        skip = 8;
+    } else if (numDays >= 7) {
+        skip = 4;
+    } else if (numDays >= 3) {
+        skip = 2;
     }
-    for (var i = 0; i < numPoints; i++) {
-        var currDate = new Date(currRounded);
-        var formatted = currDate.toLocaleString('en-US', {hour: 'numeric',minute: 'numeric',hour12: true});
-        xLabels.unshift(formatted);
-        timestamps.push(currRounded);
-        currRounded -= interval*60000; // decrement by interval mins
+
+    for (let i = 0; i < data.length; i+=skip) {
+        let currTime = new Date(data[i]);
+        if (i == data.length-1 || new Date(data[i+1]).getDay() != currDay.getDay()) { // the next day is a new day or we're at the end
+            if (numDays == 1) {
+                break;
+            }
+            let formatted = currTime.toLocaleString('en-US', { month: 'numeric', day: 'numeric', hour12: true });
+            xLabels.unshift(formatted);
+            if (i != data.length-1) {
+                currDay = new Date(data[i+1]);
+                dayCount++;
+            }
+        } else {
+            if (numDays == 1) {
+                let formatted = currTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+                xLabels.unshift(formatted);
+            } else {
+                xLabels.unshift("");
+            }
+        }
+        timestamps.push(currTime);
+        if (dayCount > numDays) {
+            break;
+        }
     }
-    return {timestamps,xLabels};
+    
+    return { timestamps, xLabels };
 }
 
-function parseData(all_data,interval,timestamps) {
-    var data = Array(timestamps.length).fill(null)
+function parseData(all_data, interval, timestamps) {
+    let data = Array(timestamps.length).fill(null)
     if (all_data != []) {
-        for (var i = 0; i < all_data.length; i++) {
-            var curr = all_data[i];
-            var roundedTimestamp = Math.round(Date.parse(curr.timestamp) / 1000 / 60 / interval) * interval * 60 * 1000;
-            var foundTimestampIndex = timestamps.findIndex(t => t == roundedTimestamp);
+        for (let i = 0; i < all_data.length; i++) {
+            let curr = all_data[i];
+            let roundedTimestamp = Math.round(Date.parse(curr.timestamp) / 1000 / 60 / interval) * interval * 60 * 1000;
+            let foundTimestampIndex = timestamps.findIndex(t => t == roundedTimestamp);
             if (foundTimestampIndex != -1) {
                 data[foundTimestampIndex] = curr;
             }
@@ -46,7 +72,6 @@ export default class App extends React.Component {
             all_data: [],
             moistureData: [],
             temperatureData: [],
-            methaneData: [],
             xLabels: [],
             data: {
                 labels: this.xLabels,
@@ -55,7 +80,7 @@ export default class App extends React.Component {
                         fill: false,
                         backgroundColor: 'rgb(92, 165, 197)',
                         borderColor: 'rgb(92, 165, 197)',
-                        borderWidth: 4,
+                        borderWidth: 2,
                         data: this.moistureData,
                         yAxisID: 'A'
                     },
@@ -64,18 +89,9 @@ export default class App extends React.Component {
                         fill: false,
                         backgroundColor: 'rgb(229, 181, 9)',
                         borderColor: 'rgb(229, 181, 9)',
-                        borderWidth: 4,
+                        borderWidth: 2,
                         data: this.temperatureData,
                         yAxisID: 'B'
-                    },
-                    {
-                        label: 'Methane',
-                        fill: false,
-                        backgroundColor: 'rgb(65, 154, 79)',
-                        borderColor: 'rgb(65, 154, 79)',
-                        borderWidth: 4,
-                        data: this.methaneData,
-                        yAxisID: 'C'
                     }
                 ]
             },
@@ -103,31 +119,28 @@ export default class App extends React.Component {
                             title: function(tooltipItems, data) {
                                 return '';
                             },
-                            label: function(context) {  
+                            label: function(context) {
                                 const metricName = context.dataset.label;
-                                var tooltipLabel = context.parsed.y;
+                                let tooltipLabel = context.parsed.y;
+                                console.log("label index " + context.parsed.x)
                                 if (metricName === "Moisture") {
                                     tooltipLabel += "%";
                                 } else if (metricName === "Temperature") {
                                     tooltipLabel += "°C";
-                                } else if (metricName === "Methane") {
-                                    tooltipLabel += "ppm";
                                 }
                                 return tooltipLabel;
                             },
                             labelTextColor: function(context) {
                                 const metricName = context.dataset.label;
-                                var color = 'rgba(0,0,0,1)';
+                                let color = 'rgba(0,0,0,1)';
                                 if (metricName === "Moisture") {
                                     color = 'rgba(92,165,197,1)';
                                 } else if (metricName === "Temperature") {
                                     color = 'rgba(229,181,9,1)';
-                                } else if (metricName === "Methane") {
-                                    color = 'rgba(65,154,79,1)';
                                 }
 
                                 return color;
-                                
+
                             },
                         }
                     }
@@ -137,121 +150,135 @@ export default class App extends React.Component {
                         type: 'linear',
                         display: true,
                         position: 'left',
+                        min: 0,
+                        max: 100,
+                        title: {
+                            display: true,
+                            text: 'Moisture (%)'
+                        },
+                        grid: {
+                            display: false
+                        },
                         ticks: {
                             beginAtZero: true,
+                            stepSize: 10
                         },
                     },
                     B: {
                         type: 'linear',
                         display: true,
                         position: 'right',
+                        min: 50,
+                        max: 120,
+                        title: {
+                            display: true,
+                            text: 'Temperature (°C)'
+                        },
+                        grid: {
+                            display: false
+                        },
                         ticks: {
                             beginAtZero: true,
+                            precision: 1
                         },
-                    },
-                    C: {
-                        type: 'linear',
-                        display: false,
-                        position: 'left',
-                        ticks: {
-                            beginAtZero: true,
-                        },
-                    },
-
+                    }
                 },
             }
 
         };
         this.handleTimescaleChange = this.handleTimescaleChange.bind(this);
+        this.make_request = this.make_request.bind(this);
     };
 
-    updateData(interval,numPoints) {
-        var {timestamps,xLabels} = generateXLabelsAndTimestamps(interval,numPoints)
-        var new_data = parseData(this.state.all_data,interval,timestamps)
-        var moistureData = []
-        var temperatureData = []
-        var methaneData = []
-        for (var i = 0; i < new_data.length; i++) {
-            var curr = new_data[i];
-            if (curr != null) {
-                moistureData.push(curr.moisture)
-                temperatureData.push(Math.round(curr.temperature * 10) / 10)
-                methaneData.push(curr.methane)
-            } else {
-                moistureData.push(null)
-                temperatureData.push(null)
-                methaneData.push(null)
-            }
-        }
-        var data = {...this.state.data}
+    displayResults(resultsString, numDays) {
+        // Convert the JSON string into JS objects
+        let resultsJS = JSON.parse(resultsString);
+
+        let data = {...this.state.data }
+        data.labels = resultsJS.dateTimeData;
+        data.datasets[0].data = resultsJS.moistureData
+        data.datasets[1].data = resultsJS.temperatureData
+        let { timestamps, xLabels } = generateXLabelsAndTimestamps(numDays, resultsJS.dateTimeData)
         data.labels = xLabels;
-        data.datasets[0].data = moistureData
-        data.datasets[1].data = temperatureData
-        data.datasets[2].data = methaneData
         this.setState({
             xLabels: xLabels,
             data: data
         })
+    };
+
+    make_request(fn) {
+        // Make a HTTP request via AJAX to Node server 
+        let httpRequest = new XMLHttpRequest();
+        httpRequest.open("GET", "https://compost-o-matic.herokuapp.com/getData");
+        httpRequest.send();
+        httpRequest.onreadystatechange = async function() {
+            console.log(httpRequest.readyState);
+            if (httpRequest.readyState == 4) {
+                if (httpRequest.status == 200) {
+                    console.log("response" + httpRequest.responseText);
+                    fn(httpRequest.responseText);
+                } else {
+                    alert("AJAX error!!!");
+                    console.log(httpRequest.status);
+                }
+            }
+        }
     }
 
+
     handleTimescaleChange = (value) => {
-        var interval = 15;
-        var numPoints = 8;
+        let numDays = 1;
         switch (value) {
-            case "2 Hours":
-                interval = 15;
-                numPoints = 8;
-                break;
-            case "5 Hours":
-                interval = 15;
-                numPoints = 20;
-                break;
-            case "10 Hours":
-                interval = 15;
-                numPoints = 40;
-                break;
             case "Day":
-                interval = 30;
-                numPoints = 48;
+                numDays = 1;
                 break;
-            case "All":
-                interval = 30;
-                numPoints = 48*2;
+            case "Week":
+                numDays = 7;
+                break;
+            case "Month":
+                numDays = 30;
                 break;
         }
-        this.updateData(interval,numPoints);
+        this.make_request(function(returnVal){
+            console.log("returnval"+returnVal);
+            this.displayResults(returnVal, numDays);
+        }.bind(this));
     }
 
     async componentDidMount() {
         this.setState({
             all_data: await fetchData()
         })
-        this.updateData(15,8);
+        this.make_request(function(returnVal){
+            console.log("returnval"+returnVal);
+            this.displayResults(returnVal, 1);
+        }.bind(this));
     }
 
 
     render() {
         defaults.font.family = "'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif"
         defaults.font.size = 15
-        
-        return ( 
-            <div>
-                <div className = "App" >
-                    < header className = "App-header" >
-                        < img src = { logo }
-                            className = "App-logo"
-                            alt = "logo" / >
-                        <p >Welcome to the Compost-O-Matic Dashboard!</p>
-                    </header> 
-                </div>
 
-                <div className = "chart" >
-                    <Dropdown onChange={this.handleTimescaleChange} />
-                    <Line data = { this.state.data }
-                        options = { this.state.options }
-                        redraw={true} />
-                </div>
+        return (
+            <div>
+            <div className = "App" >
+                < header className = "App-header" >
+                    < img src = { logo }
+                        className = "App-logo"
+                        alt = "logo" / >
+                    <p >Welcome to the Compost-O-Matic Dashboard!</p>
+                </header> 
             </div>
+
+            <div className = "chart" >
+                <Dropdown onChange={this.handleTimescaleChange} />
+                <Line data = { this.state.data }
+                    options = { this.state.options }
+                    redraw={true} />
+            </div>
+        </div>
+
         );
     }
 }
--- client/src/Dropdown.js
+++ client/src/Dropdown.js
@@ -3,38 +3,43 @@ import PropTypes from 'prop-types';
 import './App.css';
 
 class Dropdown extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-  static defaultProps = {
-    options: ["2 Hours","5 Hours","10 Hours","Day","All"]
-  }
+    static defaultProps = {
+        options: ["Day", "Week", "Month", ]
+    }
 
-  handleChange(event) {
-    this.props.onChange(event.target.value);
-  }
+    handleChange(event) {
+        this.props.onChange(event.target.value);
+    }
 
-  render() {
-    let timescaleOptions = this.props.options.map(timescale => {
-      return <option key={timescale} value={timescale}>{timescale}</option>
-    });
-    return (
-      <div className="dropdown">
-        <label></label><br />
-          <select id="soflow" ref="timescale" value={this.props.value} onChange={this.handleChange}>
-            {timescaleOptions}
-          </select>
-      </div>
-    );
-  }
+    render() {
+        let timescaleOptions = this.props.options.map(timescale => {
+            return <option key = { timescale }
+            value = { timescale } > { timescale } < /option>
+        });
+        return ( <
+            div className = "dropdown" >
+            <
+            label > < /label><br / >
+            <
+            select id = "soflow"
+            ref = "timescale"
+            value = { this.props.value }
+            onChange = { this.handleChange } > { timescaleOptions } <
+            /select> < /
+            div >
+        );
+    }
 }
 
 Dropdown.propTypes = {
-  onChange: PropTypes.func,
-  options: PropTypes.array,
-  value: PropTypes.string
+    onChange: PropTypes.func,
+    options: PropTypes.array,
+    value: PropTypes.string
 };
 
 export default Dropdown;
