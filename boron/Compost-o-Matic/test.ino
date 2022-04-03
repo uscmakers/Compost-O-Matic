a7864d921cfa31d2694330fad2f84b8b4bdc74a6
Jenny Kim <yjkim@usc.edu>
2022-04-03 15:45:57 -0700

    with sleeping functionality

--- boron/Compost-o-Matic/test.ino
+++ boron/Compost-o-Matic/test.ino
@@ -12,64 +12,80 @@ const uint32_t msSAMPLE_INTERVAL = 2500;
 // define publish rate (10s)
 const uint32_t msMETRIC_PUBLISH  = 10000;
 // define sleep time
-const unsigned long SLEEP_TIME = 60000 * 0;
+const unsigned long SLEEP_TIME = 60000 * 0; // 60 seconds
 
 double soilMoisturePercent;
 
 const int AirValue = 3670;   // our definition of air value
 const int WaterValue = 800;  // our definition of water value
+SystemSleepConfiguration config;
 
-unsigned long prevSleepTimer = 0;
-
+enum states { S_SOIL, S_TEMPERATURE, S_PUBLISH, S_SLEEP };
+states currState = S_SOIL;
 void setup() {
   pinMode(TEMPERATURE, INPUT);
   pinMode(SOIL, INPUT);
   Serial.begin(9600);
-  sleepConfigNormal.mode(SystemSleepMode::ULTRA_LOW_POWER)
-                   .duration(sleepTimeNormal);
-
-  sleepConfigShort.mode(SystemSleepMode::ULTRA_LOW_POWER)
-                   .duration(sleepTimeShort);
-
+  config.mode(SystemSleepMode::ULTRA_LOW_POWER)
+    .gpio(D2, FALLING)
+    .duration(1min);
 }
 
-void loop() {
-  unsigned long currSleep = millis();
-  // get sample every msSAMPLE_INTERVAL seconds
-  if (millis() - msLastSample >= msSAMPLE_INTERVAL){
-    getTemp();
-    getSoil();
-    publishData();
+void updateState() {
+  switch(currState) {
+      case S_SOIL:
+        currState = S_TEMPERATURE;
+        getSoil();
+        break;
+      case S_TEMPERATURE:
+        currState = S_PUBLISH;
+        getTemp();
+        break;
+      case S_PUBLISH:
+        currState = S_SLEEP;
+        publishData();
+        break;
+      case S_SLEEP:
+        currState = S_SOIL;
+        System.sleep(config);
+        break;
   }
-  // if (millis() - prevSleepTimer >= SLEEP_INTERVAL) {
-  //   // put boron to sleep!
-  // }
+}
+void loop() {
+  if(Particle.connected()) {
+    updateState();
+  }
 }
 
 // read data from soil sensor
 void getSoil() {
   double val = analogRead(SOIL);  
   soilMoisturePercent = (100 * ( 1 - (double) (val - WaterValue) / (double) AirValue));
+  Serial.println("Soil reading measuring" + String(soilMoisturePercent));
 }
 
 // read data from temperature sensor
 void getTemp(){
   float _temp;
   int i = 0;
-  do {
-    _temp = temp_sensor.getTemperature();
-  } while (!temp_sensor.crcCheck() && MAXRETRY > i++);
 
-  if (i < MAXRETRY) {
-    celsius = _temp;
-    fahrenheit = temp_sensor.convertToFahrenheit(_temp);
-    Serial.println(fahrenheit);
-  }
-  else {
-    celsius = fahrenheit = NAN;
-    Serial.println("Invalid reading");
-  }
-  msLastSample = millis();
+  _temp = temp_sensor.getTemperature();
+  fahrenheit = temp_sensor.convertToFahrenheit(_temp);
+  // Serial.println("Temperature measure" + String(fahrenheit));
+  // do {
+  //   _temp = temp_sensor.getTemperature();
+  // } while (!temp_sensor.crcCheck() && MAXRETRY > i++);
+
+  // if (i < MAXRETRY) {
+  //   celsius = _temp;
+  //   fahrenheit = temp_sensor.convertToFahrenheit(_temp);
+  //   // Serial.println(fahrenheit);
+  // }
+  // else {
+  //   celsius = fahrenheit = NAN;
+  //   // Serial.println("Invalid reading");
+  // }
+  // msLastSample = millis();
 }
 
 // publish data in JSON format
@@ -77,5 +93,5 @@ void publishData(){
   String data = String::format(
     "{ \"temperatureData\": %s,  \"moistureData\": %s}",
     fahrenheit, soilMoisturePercent);
-  Particle.publish("testAdd", data, PRIVATE);  
+  Particle.publish("testAdd", data, PRIVATE);
 }
